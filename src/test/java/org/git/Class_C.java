382f9218bf04bb2c2a5ba3e0b28dba5519b24aa6
Giri13Ravi <ican13giri@gmail.com>
2024-04-11 16:41:21 +0530

    I just pushing the Class_C code

---
+++ src/test/java/org/git/Class_C.java
@@ -0,0 +1,21 @@
+package org.git;
+
+public class Class_C {
+
+	private void Basic_1() {
+		System.out.println("Core-Java");
+
+	}
+	
+	private void Basic_2() {
+		System.out.println("Selenium");
+
+	}
+	
+	private void Code_Sharing() {
+		System.out.println("Git");
+	}
+
+	
+
+}
